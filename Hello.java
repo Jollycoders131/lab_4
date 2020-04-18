f3b574a2b31f425b5c49357831e74a62ca708d48
Jollycoders131 <jamesgouveia@csus.edu>
2020-04-17 17:51:19 -0700

    Initial Commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("James Gouveia - This line was added by James.");
+		// Add a new line with your information
+}
+
+}
