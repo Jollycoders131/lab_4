39d50ad8d9be5ce36d8f44c497bb86676c2cebac
nick <nick@10.0.0.187>
2020-04-17 19:51:50 -0700

    Changed by Nick

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
 		System.out.println("James Gouveia - This line was added by James.");
+		System.out.println("Nick Goodie - This line was added by Nick.");
 		// Add a new line with your information
 }
 
