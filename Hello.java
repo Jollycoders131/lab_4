7291e63c131d54493ba165ff71620f623af138c6
Tejpal Lalihiana <tejpalssb23@gmail.com>
2020-04-18 11:59:07 -0700

    Changed by Tejpal added semicolon

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("James Gouveia - This line was added by James.");
 		System.out.println("Nick Goodie - This line was added by Nick.");
-		System.out.println("Tejpal Lalihiana - This line was added by Tejpal.");
+		System.out.println("Tejpal Lalihiana - This line was added by Tejpal");
 		// Add a new line with your information
 }
 
